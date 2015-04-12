2e58ed4036a510660ee1b68143bcacf0261926ad
beepscore <support@beepscore.com>
2015-04-12 10:05:35 -0700

    Add DogViewController

---
+++ KVOPractice/DogViewController.h
@@ -0,0 +1,13 @@
+//
+//  DogViewController.h
+//  KVOPractice
+//
+//  Created by Steve Baker on 4/12/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DogViewController : UIViewController
+
+@end
---
+++ KVOPractice/DogViewController.m
@@ -0,0 +1,37 @@
+//
+//  DogViewController.m
+//  KVOPractice
+//
+//  Created by Steve Baker on 4/12/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import "DogViewController.h"
+
+@interface DogViewController ()
+
+@end
+
+@implementation DogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
