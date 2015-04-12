2cced63fba752dd86d4c06ac15acd58d444c6969
beepscore <support@beepscore.com>
2015-04-12 11:29:19 -0700

    Rename ViewController to StatusViewController. Add method unwindToStatusViewControllerViaSegue:
    In storyboard, in StatusViewController add button to segue to DogViewController.
    In storyboard, in DogViewController add button to do unwind segue.

--- KVOPractice/StatusViewController.h
+++ KVOPractice/StatusViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  StatusViewController.h
 //  KVOPractice
 //
 //  Created by Steve Baker on 4/11/15.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface StatusViewController : UIViewController
 
 
 @end
---
+++ KVOPractice/StatusViewController.m
@@ -0,0 +1,37 @@
+//
+//  StatusViewController.m
+//  KVOPractice
+//
+//  Created by Steve Baker on 4/11/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import "StatusViewController.h"
+
+@interface StatusViewController ()
+
+@end
+
+@implementation StatusViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// Use unwind segue. It's very flexible.
+// In storyboard, another view controller's exit unwind segue
+// can call this view controller's IBAction method
+// http://stackoverflow.com/questions/12561735/what-are-unwind-segues-for-and-how-do-you-use-them
+- (IBAction)unwindToStatusViewControllerViaSegue:(UIStoryboardSegue *)unwindSegue {
+
+    if ([unwindSegue.identifier isEqualToString:@"unwindDogToStatusSegue"]) { NSLog(@"unwindDogToStatusSegue");
+    }
+}
+
+@end
